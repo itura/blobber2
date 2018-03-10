0a6dc7df06781d3797045de93b50867a4816b096
Austin Garrard <agarrard@thoughtworks.com>
2018-03-10 08:47:20 -0500

    Basic mouse following

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, {Component} from 'react';
 import './App.css';
+import {Observable} from 'rxjs';
+
+const style = function (x, y) {
+  return {
+    position: 'absolute',
+    left: x,
+    top: y,
+    WebkitTransition: 'left .5s, top .5s',
+    transition: 'left .5s, top .5s',
+    'transitionTimingFunction': 'ease-out'
+  }
+};
 
 class App extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      style: {}
+    }
+  }
+
+  componentDidMount() {
+    Observable.fromEvent(window, 'mousemove')
+      .sampleTime(100)
+      .subscribe(e => {
+        this.setState((prevState, props) => ({
+          style: style(e.clientX, e.clientY)
+        }));
+      });
+  }
+
   render() {
     return (
-      <div>
+      <div style={this.state.style}>
         Hello
       </div>
     );
