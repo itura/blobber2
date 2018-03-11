976099d6a75b808527f0c8b19342b3bd3776b68d
Austin Garrard <agarrard@thoughtworks.com>
2018-03-11 15:17:09 -0400

    Event bus adapter for server

--- server.js
+++ server.js
@@ -1,52 +1,63 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const Observable = require('rxjs').Observable;
-let bus;
+let rs = require('rxjs');
+const Observable = rs.Observable;
+const Subject = rs.Subject;
 
-let newId = 0;
-
+// helpers
 function createLocation(x, y) {
-  return {x, y,
+  return {
+    x, y,
     changeBy(dx, dy) {
       return createLocation(x + dx, y + dy);
     }
   };
 }
 
+
+// event bus
+function createEventBus(source) {
+  return {
+    notify(type, data) {
+      io.sockets.emit(type, data);
+    },
+    get(type) {
+      return source
+        .filter(event => event.type === type)
+        .map(event => event.data);
+    }
+  }
+}
+
+const eventSource = new Subject();
+const eventBus = createEventBus();
+const eventTypes = ['initialize', 'move'];
+
+// initial game state
 const blobs = [];
-[1,2,3,4,5].forEach((_, index) => {
+let newId = 0;
+function createBlob(x, y, size) {
+  newId += 1;
+  let blob = {id: newId, location: createLocation(x, y), size};
+  blobs.push(blob);
+  return blob;
+}
+[1, 2, 3, 4, 5].forEach((_, index) => {
   const blob = createBlob(100 * index, 100 * index, 20)
   Observable.timer(1000, 2000)
     .map(value => ({id: blob.id, location: blob.location.changeBy(value * 10, value * 10)}))
     .subscribe(data => {
-      if (bus) {
-        bus.notify('move', data)
-      }
+      eventBus.notify('move', data)
     });
 });
 
-function createBlob(x, y, size) {
-  newId += 1;
-  let blob = {id: newId, location: createLocation(x,y), size};
-  blobs.push(blob);
-  return blob;
-}
-
-
+// server monitoring
 Observable.timer(1000, 2000)
   .subscribe(value => console.log('blobs:', blobs.length, blobs.map(blob => blob.id)));
 
 io.on('connection', socket => {
-  bus = {
-    notify(type, data) {
-      socket.broadcast.emit(type, data);
-    },
-    subscribeTo(type, fn) {
-      socket.on(type, fn);
-    }
-  };
-
+  // create a new player
   const player = createBlob(-100, -100, 100);
   console.log(`User ${player.id} connected.`);
 
@@ -58,9 +69,8 @@ io.on('connection', socket => {
     blobs: blobs.filter(blob => blob !== player)
   });
 
-  socket.on('move', data => {
-    console.log('move!', data);
-  });
+  // route events coming in on this socket to the main event bus
+  eventTypes.forEach(type => socket.on(type, data => eventSource.next({type, data})));
 });
 
 http.listen(5000, function () {
