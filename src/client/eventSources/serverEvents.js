f20962b0b683a3f401c0d751d18b11d6a24825ab
Austin Garrard <agarrard@thoughtworks.com>
2018-04-21 18:00:58 -0400

    Extract socket listening logic

--- src/client/eventSources/serverEvents.js
+++ src/client/eventSources/serverEvents.js
@@ -1,42 +1,31 @@
 import openSocket from 'socket.io-client'
-import { Subject } from 'rxjs/Subject'
-import { filter, map } from 'rxjs/operators'
+import { filter } from 'rxjs/operators'
+import { createObservableSocket } from '../../shared/observableSocket'
 
 export function Event (type, data) {
   return {type, data}
 }
 
+const filterByIdIfGiven = id => source$ => id
+  ? source$.pipe(filter(event => event.id === id))
+  : source$
+
 function createServerEvents () {
-  const bus$ = new Subject()
   const socket = openSocket('http://localhost:5000/', {
     reconnection: false
   })
 
-  socket.on('error', data => {
-    console.log('error!', data)
-    socket.disconnect()
+  const os = createObservableSocket(socket, {
+    eventTypes: ['init', 'pm', 'np', 'rv', 'ch']
   })
-
-  const eventTypes = ['init', 'pm', 'np', 'rv', 'ch']
-  eventTypes.forEach(type => {
-    socket.on(type, data => bus$.next(Event(type, data)))
-  })
-
-  const filterByIdIfGiven = id => source$ => id
-    ? source$.pipe(filter(event => event.id === id))
-    : source$
 
   return {
     get (type, id = null) {
-      return bus$.pipe(
-        filter(event => event.type === type),
-        map(event => event.data),
-        filterByIdIfGiven(id)
-      )
+      return os.on(type, filterByIdIfGiven(id))
     },
 
     notify (type, data) {
-      socket.emit(type, data)
+      os.emit(type, data)
     }
   }
 }
---
+++ src/shared/observableSocket.js
@@ -0,0 +1,57 @@
+import { Subject } from 'rxjs/Subject'
+import { filter, map } from 'rxjs/operators'
+
+export function createObservableSocket (socket, ...config) {
+  let eventTypes = null
+  let operators = []
+
+  if (typeof config[0] === 'string') {
+    eventTypes = config
+  } else if (typeof config[0] === 'object') {
+    eventTypes = config[0].eventTypes || []
+    operators = config[0].operators || []
+  }
+
+  const source$ = new Subject()
+  eventTypes.forEach(type => {
+    socket.on(type, data => source$.next({type, data}))
+  })
+
+  socket.on('error', data => {
+    console.log('error!', data)
+    socket.close()
+  })
+
+  const match = type => source => source.pipe(
+    filter(event => event.type === type),
+    map(event => event.data)
+  )
+
+  const os = {
+    on (type, ...operators) {
+      return source$.pipe(match(type), ...operators)
+    },
+
+    emit (type, data) {
+      socket.emit(type, data)
+    },
+
+    close () {
+      socket.close()
+      source$.complete()
+    }
+  }
+
+  return operators
+    .map(
+      operator => ({
+        [operator.name]: function (type, ...args) {
+          return this.get(type, operator(...args))
+        }
+      })
+    )
+    .reduce(
+      (os, operator) => Object.assign(os, operator),
+      os
+    )
+}
