576b6e0fdcf4aaabd8dfce43c86187097fffb045
Austin Garrard <agarrard@thoughtworks.com>
2018-05-07 00:03:07 -0400

    Manage subscriptions in KeyComboStream

--- src/client/events/userInput/keyComboStream.js
+++ src/client/events/userInput/keyComboStream.js
@@ -3,6 +3,8 @@ import { LockedSubject } from '../../../shared/subjects'
 import { merge } from 'rxjs/observable/merge'
 import { KeyCombo } from './keys'
 import { unless } from '../../../shared/operators'
+import { Observable } from 'rxjs/observable'
+import { Subscription } from 'rxjs'
 
 /**
  *
@@ -15,7 +17,7 @@ import { unless } from '../../../shared/operators'
  * will be emitted. Once the latest emission from all of the lock streams
  * is false again, the KeyComboStream will resume emitting events.
  *
- * @param {Array<number>} keys - The keys to listen for
+ * @param {Array<number>} keys - The key codes to listen for
  * @param {Observable} blur$ - window blur event stream
  * @param {Observable} keyDown$ - window keydown event stream
  * @param {Observable} keyUp$ - window keyup event stream
@@ -23,59 +25,64 @@ import { unless } from '../../../shared/operators'
  * @returns Observable
  */
 export function KeyComboStream (keys, blur$, keyDown$, keyUp$, locks) {
-  let currentKeyCombo = KeyCombo()
+  return new Observable(observer => {
+    let currentKeyCombo = KeyCombo()
+    const subscription = new Subscription()
 
-  // Remove any keys in the combo when the user alt-tabs
-  blur$.subscribe(event => {
-    currentKeyCombo = currentKeyCombo.clear()
+    // Remove any keys in the combo when the user alt-tabs
+    subscription.add(blur$.subscribe(event => {
+      currentKeyCombo = currentKeyCombo.clear()
+    }))
+
+    //
+    // Set up stream for keydown events
+    //
+
+    const addKeyIfRelevant = source$ => source$.pipe(
+      filter(event => keys.includes(event.keyCode)),
+      map(event => currentKeyCombo.add(event.keyCode)),
+      filter(newKeyCombo => !newKeyCombo.equals(currentKeyCombo))
+    )
+
+    const _keyDown$ = keyDown$.pipe(
+      addKeyIfRelevant,
+      share())
+
+    subscription.add(_keyDown$.subscribe(newKeyCombo => {
+      currentKeyCombo = newKeyCombo
+    }))
+
+    //
+    // Set up stream for keyup events
+    //
+
+    const removeKeyIfRelevant = source$ => source$.pipe(
+      filter(event => keys.includes(event.keyCode)),
+      map(event => currentKeyCombo.remove(event.keyCode)),
+      tap(newKeyCombo => { currentKeyCombo = newKeyCombo })
+    )
+
+    const _keyUp$ = keyUp$.pipe(
+      removeKeyIfRelevant,
+      share())
+
+    subscription.add(_keyUp$.subscribe(newKeyCombo => {
+      currentKeyCombo = newKeyCombo
+    }))
+
+    //
+    // Set up stream for locking event emission
+    //
+
+    const isLocked$ = LockedSubject()
+    locks.forEach(lock => subscription.add(lock.subscribe(isLocked$.observer())))
+
+    //
+    // Cross the streams D:
+    //
+
+    subscription.add(merge(_keyDown$, _keyUp$).pipe(unless(isLocked$)).subscribe(observer))
+
+    return subscription
   })
-
-  //
-  // Set up stream for keydown events
-  //
-
-  const addKeyIfRelevant = source$ => source$.pipe(
-    filter(event => keys.includes(event.keyCode)),
-    map(event => currentKeyCombo.add(event.keyCode)),
-    filter(newKeyCombo => !newKeyCombo.equals(currentKeyCombo))
-  )
-
-  const _keyDown$ = keyDown$.pipe(
-    addKeyIfRelevant,
-    share())
-
-  _keyDown$.subscribe(newKeyCombo => {
-    currentKeyCombo = newKeyCombo
-  })
-
-  //
-  // Set up stream for keyup events
-  //
-
-  const removeKeyIfRelevant = source$ => source$.pipe(
-    filter(event => keys.includes(event.keyCode)),
-    map(event => currentKeyCombo.remove(event.keyCode)),
-    tap(newKeyCombo => { currentKeyCombo = newKeyCombo })
-  )
-
-  const _keyUp$ = keyUp$.pipe(
-    removeKeyIfRelevant,
-    share())
-
-  _keyUp$.subscribe(newKeyCombo => {
-    currentKeyCombo = newKeyCombo
-  })
-
-  //
-  // Set up stream for locking event emission
-  //
-
-  const isLocked$ = LockedSubject()
-  locks.forEach(lock => lock.subscribe(isLocked$.observer()))
-
-  //
-  // Cross the streams D:
-  //
-
-  return merge(_keyDown$, _keyUp$).pipe(unless(isLocked$))
 }
